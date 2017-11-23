fe9dcdc4723c65e3d0543b24e47c83ffeae46755
Jose Angel Lopez <chepo_nr@comunidad.unam.mx>
2017-11-23 01:07:59 -0600

    add serverjs

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+
+/*
+app.get('/', function (req, res) {
+	res.render('index');
+})
+*/
+app.listen(8080, function(err) {
+	if (err) return  console.log('Error'), process.exit(1);
+	console.log('Success');
+})
