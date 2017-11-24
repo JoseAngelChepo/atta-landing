e6c34e7729c57880bf9395edf65fb760d90d3877
Jose Angel Lopez <chepo_nr@comunidad.unam.mx>
2017-11-24 01:29:07 -0600

    active index default

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ var app = express();
 
 app.use(express.static('public'));
 
-/*
+
 app.get('/', function (req, res) {
 	res.render('index');
 })
-*/
+
 app.listen(8080, function(err) {
 	if (err) return  console.log('Error'), process.exit(1);
 	console.log('Success');
