46541296191c89bb56c4f5d4bbacb10ef8cb3b96
Jose Angel Lopez <chepo_nr@comunidad.unam.mx>
2017-11-24 01:51:52 -0600

    fixed server.js

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('public'));
+app.set('port', (process.env.PORT || 5000));
 
+app.use(express.static(__dirname + '/public'));
 
-app.get('/', function (req, res) {
-	res.render('index');
-})
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
-app.listen(8080, function(err) {
-	if (err) return  console.log('Error'), process.exit(1);
-	console.log('Success');
-})
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
